ec4ce316a4ebf1a0ee8e6f216632c376b81a7ee7
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-11 18:35:29 +0200

    modal working but showing all modals

--- js/app.js
+++ js/app.js
@@ -2,20 +2,34 @@
  * Created by debbieobrien on 10/07/2017.
  */
 let personHTML;
+let personModalHTML;
+let allData = [];
 $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
+
+        //console.log(allData[0]);
         $.each( data.results, function( i, person ) {
+            //allData = data;
+            //console.log(allData)
+            console.log(person)
+            allData[person['id']['value']] = person;
+            //console.log(allData)
+
             displayData(person)
+
         });
         let $person = $('.person');
         //console.log($person);
         $person.on("click", function(e){
             e.preventDefault();
            // displayDataModal(this);
-
-
+            let selectedPerson = $('id').val();
+            console.log(selectedPerson)
+            $.each( data.results, function( i, person ) {
+            displayDataModal(person)
+            });
             $overlay.show();
         });
 
@@ -23,8 +37,10 @@ $.ajax({
 
 });
 
+
 function displayData(person){
     personHTML += '<div class="person"><a href="#">';
+    personHTML += '<div id="id" value="' + person.login.md5 + '"></div>';
     personHTML += '<img src ="' + person.picture.large + '">';
     personHTML += '<div><span class="name">' + person.name.first;
     personHTML += ' ' + person.name.last + '</span>';
@@ -34,18 +50,18 @@ function displayData(person){
     $('#people').html(personHTML);
 }
 function displayDataModal(person){
-    personHTML += '<a href="#"><div class="person">';
-    personHTML += '<p><span>Name: ' + person.name.first + ' </span>';
-    personHTML += '<span>' + person.name.last + '</span></p>';
-    personHTML += '<p> Email: ' + person.email + '</p>';
-    personHTML += '<p> Location: ' + person.location.city + '</p>';
-    personHTML += '<img src ="' + person.picture.large + '">';
-    personHTML += '<p> Username: ' + person.login.username + '</p>';
-    personHTML += '<p> CellNumber: ' + person.cell + '</p>';
-    personHTML += '<p> BirthDate: ' + person.dob + '</p>';
-    personHTML += '<p> address: ' + person.location.street + ', ' + person.location.city + ', ' + person.location.state + ', ' + person.location.postcode + '</p>';
-    personHTML += '</div></a>';
-    $('#modal').html(personHTML);
+    personModalHTML += '<a href="#"><div class="person">';
+    personModalHTML += '<p><span>Name: ' + person.name.first + ' </span>';
+    personModalHTML += '<span>' + person.name.last + '</span></p>';
+    personModalHTML += '<p> Email: ' + person.email + '</p>';
+    personModalHTML += '<p> Location: ' + person.location.city + '</p>';
+    personModalHTML += '<img src ="' + person.picture.large + '">';
+    personModalHTML += '<p> Username: ' + person.login.username + '</p>';
+    personModalHTML += '<p> CellNumber: ' + person.cell + '</p>';
+    personModalHTML += '<p> BirthDate: ' + person.dob + '</p>';
+    personModalHTML += '<p> address: ' + person.location.street + ', ' + person.location.city + ', ' + person.location.state + ', ' + person.location.postcode + '</p>';
+    personModalHTML += '</div></a>';
+    $('#modal').html(personModalHTML);
 }
 
 let $overlay = $('<div id="overlay"></div>');
