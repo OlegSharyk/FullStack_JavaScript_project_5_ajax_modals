c0a2bc591aedc7c2a8247fed917461a970e87ae1
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-13 17:52:21 +0200

    prev and next need to be fixed search needs to be worked on

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ let personHTML ='';
 let personModalHTML ='';
 let $overlay = $('<div id="overlay"></div>');
 let $modal = $('<div id="modal"></div>');
-let employeeList = [];
+let employeeList = [];  //array to store the data from the api
+let filteredList = [];  //array to store the filtered data from the search
 
 const getEmployeeData = (data) =>{
     //get the data for each employee and call the displayData function to display it
@@ -14,38 +15,89 @@ const getEmployeeData = (data) =>{
         employeeList.push($(this));
         employeeList[key].push({employeeID: key});
     });
-    $.each(employeeList, function (key) {
-        displayData(employeeList[key][0], employeeList[key][1]);
-    });
 
-    $('#people').append(personHTML);
+    loadAllEmployees();
+    loadModal();
 
+};
+
+const loadModal = () => {
     $('.person').on("click", function(e){
         e.preventDefault();
         let employeeID = $(this).children('#id').val();
 
-        $.each(employeeList, function( key, person ) {
-            let personID = employeeList[key][1]
-            $.each(personID, function( key, person ) {
+        $.each(employeeList, function(key) {
+            let personID = employeeList[key][1];
+            $.each(personID, function(key) {
                 let personIDValue = personID[key];
-                    if(Number(employeeID) === personIDValue){
-                        //displayDataModal(employeeList);
-                        console.log(personIDValue)
-                        console.log(employeeID)
-                        console.log('ok')
-                        displayDataModal(employeeList[employeeID][0]);
-                    }
-
-
+                employeeID = Number(employeeID);
+                if(employeeID === personIDValue){
+                    displayDataModal(employeeList[employeeID][0]);
+                }
             })
         });
         $('#modal').html(personModalHTML);
         //show the modal and overlay on click
         $overlay.show();
         closeModal();
-        nextModal();
+        nextModal(employeeID);
+        prevModal(employeeID);
     });
 };
+
+const loadAllEmployees = () => {
+    $.each(employeeList, function (key) {
+        displayData(employeeList[key][0], employeeList[key][1]);
+    });
+    $('#people').append(personHTML);
+};
+const hideAll = (employeeList) => {
+    employeeList.forEach(function (element) {
+        $('.person').hide();
+    })
+};
+
+/*const search = () => {
+        let searchValue = $('#search').val();
+
+        /!*for(let i = 0; i < $('.person').length; i+=1){
+         let firstName = employeeList[i][0].name.first;
+         let secondName = employeeList[i][0].name.last;
+         let email = employeeList[i][0].email;
+         let fullName = firstName + ' ' + secondName;
+         if(fullName.indexOf(searchValue) > -1 && searchValue || email.indexOf(searchValue) > -1){
+         $('.person').show();
+         console.log(fullName);
+         }else{
+
+         }
+         }*!/
+
+        employeeList.forEach(function (element) {
+            let firstName = $(element).find('.person').children('.name').text();
+            console.log(firstName)
+            //hideAll(employeeList);
+            if (firstName.indexOf(searchValue) > -1 && searchValue) {
+                filteredList.push($(element));
+                //console.log(filteredList)
+            }
+        })
+};*/
+
+//$('#search').keyup(search);
+
+
+
+
+function dateOfBirth(person){
+    //Reformat the date of birth
+    let dob = person.dob;
+    let day = dob.slice(8, -9);
+    let month = dob.slice(5, -12);
+    let year = dob.slice(0, 4);
+    let newDOB = day + '/' + month + '/' + year;
+    return newDOB
+}
 
 //displayData(employeeList[0]);
 function displayData(person, key){
@@ -58,47 +110,7 @@ function displayData(person, key){
     personHTML += '<p class="city">' + person.location.city + '</p></div>';
     personHTML += '</a>';
 }
-
-$.getJSON(url, getEmployeeData);
-let filteredList = [];
-$('#submit').click(function(e){
-    e.preventDefault();
-    let searchValue = $('#search').val();
-
-
-    for(let i = 0; i < $('.person').length; i+=1){
-        let firstName = employeeList[i][0].name.first;
-        let secondName = employeeList[i][0].name.last;
-        let fullName = firstName + ' ' + secondName;
-        if(fullName.indexOf(searchValue) > -1 && searchValue){
-            $('.person').show();
-            console.log(fullName);
-            //fullName.parents
-        }
-    }
-    employeeList.forEach(function(element) {
-        let firstName = $(element).find('.person').children('.name').text();
-        if (firstName.indexOf(searchValue) > -1 && searchValue){
-            filteredList.push($(element));
-            console.log(filteredList)
-            }
-
-    })
-
-});
-
-
 function displayDataModal(person){
-    //Reformat the date of birth
-    function dateOfBirth(){
-        let dob = person.dob;
-        let day = dob.slice(8, -9);
-        let month = dob.slice(5, -12);
-        let year = dob.slice(0, 4);
-        let newDOB = day + '/' + month + '/' + year;
-        return newDOB
-    }
-
     personModalHTML += '<div class="person-modal">';
     personModalHTML += '<a href="#" class="close">X</a>';
     personModalHTML += '<a href="#" class="next">next</a>';
@@ -112,7 +124,7 @@ function displayDataModal(person){
     personModalHTML += '<div class="extra-info">';
     personModalHTML += '<p>' + person.cell + '</p>';
     personModalHTML += '<p>' + person.location.street + ' ' + person.location.city + ', ' + person.location.state + ' ' + person.location.postcode + '</p>';
-    personModalHTML += '<p>Birthday: ' + dateOfBirth() + '</p>';
+    personModalHTML += '<p>Birthday: ' + dateOfBirth(person) + '</p>';
     personModalHTML += '</div>';
     personModalHTML += '</div>';
 }
@@ -132,11 +144,27 @@ function closeModal(){
         personModalHTML ='';
     });
 }
-function nextModal(){
+function nextModal(employeeID){
     $('.next').click(function(e) {
         e.preventDefault();
-        personModalHTML='';
-
-        console.log('clicked')
+        employeeID +=1;
+        if(employeeID < employeeList.length){
+            displayDataModal(employeeList[employeeID][0]);
+        }else{
+            employeeID = 0;
+        }
     });
 }
+function prevModal(employeeID){
+    $('.prev').click(function(e) {
+        e.preventDefault();
+        employeeID -=1;
+        if(employeeID > 0){
+            displayDataModal(employeeList[employeeID][0]);
+        }else{
+            employeeID = employeeList.length;
+        }
+    });
+}
+
+$.getJSON(url, getEmployeeData);
