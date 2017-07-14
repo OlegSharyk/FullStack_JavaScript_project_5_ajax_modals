8f5febe796a5fcebba1bd2f1a8076eafd058682f
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-14 20:55:42 +0200

    working version all completed

--- js/app.js
+++ js/app.js
@@ -2,10 +2,9 @@
  * Created by debbieobrien on 10/07/2017.
  */
 const url = 'https://randomuser.me/api/?nat=gb&results=12';
-let personHTML ='';
-let personModalHTML ='';
 let $overlay = $('<div id="overlay"></div>');
 let $modal = $('<div id="modal"></div>');
+let personModalHTML ='';
 let employeeList = [];  //array to store the data from the api
 let filteredList = [];  //array to store the filtered data from the search
 $('body').append($overlay); //append the overlay to the the people div
@@ -15,86 +14,53 @@ $overlay.hide(); //hide the modal and overlay
 const getEmployeeData = (data) =>{
     //get the data for each employee and call the displayData function to display it
     $.each(data.results, function(key) {
-        employeeList.push($(this));
-        employeeList[key].push({employeeID: key});
+        employeeList.push(this);
     });
 
-    loadAllEmployees(employeeList);
+    displayData(employeeList);
     loadModal(employeeList);
-
-};
-
-const loadAllEmployees = (list) => {
-    $.each(list, function (key) {
-        displayData(list[key][0], list[key][1]);
-    });
-    $('#people').append(personHTML);
 };
 
 const loadModal = (list) => {
-    $('.person').on("click", function(e){
+    $('.person').on("click", function (e) {
         e.preventDefault();
-        let employeeID = $(this).children('#id').val();
-
-        $.each(list, function(key) {
-            let personID = list[key][1]; //key is stored in second part of object
-            $.each(personID, function(key) {
-                let personIDValue = personID[key];
-                employeeID = Number(employeeID);
-                if(employeeID === personIDValue){
-                    displayDataModal(list[employeeID][0]);
-                }
-            })
-        });
-        $('#modal').html(personModalHTML);
-        //show the modal and overlay on click
-        $overlay.show();
-        closeModal();
-        nextModal(employeeID);
-        prevModal(employeeID);
-    });
-};
-
-
-const hideAll = (list) => {
-    list.forEach(function (element) {
-        $('.person').hide();
+        let id = $(this).attr('data-id');
+        displayDataModal(id, list[id], list);
     })
 };
 
 const search = () => {
-        let searchValue = $('#search').val();
+    let searchValue = $('#search').val();
+    //if search is empty show all and hide no results message
+    if (searchValue === '') {
+        displayData(employeeList);
+        loadModal(employeeList);
+        $('.no-results').hide();
+        return;
+    }
 
-       /* /!*for(let i = 0; i < $('.person').length; i+=1){
-         let firstName = employeeList[i][0].name.first;
-         let secondName = employeeList[i][0].name.last;
-         let email = employeeList[i][0].email;
-         let fullName = firstName + ' ' + secondName;
-         if(fullName.indexOf(searchValue) > -1 && searchValue || email.indexOf(searchValue) > -1){
-         $('.person').show();
-         console.log(fullName);
-         }else{
+    filteredList = []; //set to empty before we filter again
+    employeeList.forEach(function (element) {
+        //use regex to make sure name matches the the search value
+        //must start with that letter and doesnt matter if upper or lower
+        let regex = new RegExp('^' + searchValue, 'i');
 
-         }
-         }*!/*/
+        if (element.name.first.match(regex) || element.name.last.match(regex) || element.email.match(regex)) {
+            filteredList.push(element);  //push the found values into the array
+            $('.no-results').hide();
+        }
 
-        employeeList.forEach(function (element) {
-            let firstName = $(element).find('.person').children('.name').text();
-            console.log(firstName)
-            hideAll(employeeList);
-            if (firstName.indexOf(searchValue) > -1 && searchValue) {
-                filteredList.push($(element));
-                //console.log(filteredList)
-            }
-            loadAllEmployees(filteredList);
-            loadModal(filteredList);
-        })
+        if (filteredList.length === 0){
+            $('.no-results').show(); //if its empty show no results message
+            $('.no-results').text('Sorry No Employee Found');
+        }
+    });
+    displayData(filteredList); //call the function to display the data of the filtered list
+    loadModal(filteredList); //call the function to display the modal of the filtered list
+
 };
 
-$('#search').keyup(search);
-
-
-
+$('#search').on('keyup', search);
 
 function dateOfBirth(person){
     //Reformat the date of birth
@@ -106,18 +72,24 @@ function dateOfBirth(person){
     return newDOB;
 }
 
-function displayData(person, key){
-    personHTML += '<article class="person">';
-    personHTML += '<input type="hidden" id="id" value="' + key.employeeID + '">';
-    personHTML += '<img class="avatar" src ="' + person.picture.large + '">';
-    personHTML += '<div><span class="name">' + person.name.first;
-    personHTML += ' ' + person.name.last + '</span>';
-    personHTML += '<p class="email">' + person.email + '</p>';
-    personHTML += '<p class="city">' + person.location.city + '</p></div>';
-    personHTML += '</article>';
+function displayData(list){
+    let personHTML = '';  //empty it
+    $.each(list, function(key){
+        let person = list[key]; // set person to the list with each key
+        //key is added so we can keep track of how many there are
+        personHTML += '<article class="person" data-id="' + key + '">';
+        personHTML += '<img class="avatar" src ="' + person.picture.large + '">';
+        personHTML += '<div><span class="name">' + person.name.first;
+        personHTML += ' ' + person.name.last + '</span>';
+        personHTML += '<p class="email">' + person.email + '</p>';
+        personHTML += '<p class="city">' + person.location.city + '</p></div>';
+        personHTML += '</article>';
+    });
+
+    $('#people').html(personHTML);  //add the html to the people div
 }
-function displayDataModal(person){
-    personModalHTML += '<div class="person-modal">';
+function displayDataModal(id, person, list){
+    personModalHTML = '<div class="person-modal">';
     personModalHTML += '<a href="#" class="close">X</a>';
     personModalHTML += '<a href="#" class="next">></a>';
     personModalHTML += '<a href="#" class="prev"><</a>';
@@ -133,10 +105,20 @@ function displayDataModal(person){
     personModalHTML += '<p>Birthday: ' + dateOfBirth(person) + '</p>';
     personModalHTML += '</div>';
     personModalHTML += '</div>';
+
+    $('#modal').html(personModalHTML);  //add the html to the modal
+
+    if(list.length === 1){
+        $('.next').hide();
+        $('.prev').hide();
+    }
+
+    //show the modal and overlay on click
+    $overlay.show();
+    closeModal();
+    nextModal(id, list);
+    prevModal(id, list);
 }
-
-
-
 
 //Hide the modal and overlay if they click again and empty the html so as it clear it
 function closeModal(){
@@ -146,31 +128,30 @@ function closeModal(){
         personModalHTML ='';
     });
 }
-function nextModal(employeeID){
+
+//check which id is current and which list the filtered list or normal list
+function nextModal(id, list){
     $('.next').click(function(e) {
         e.preventDefault();
-        employeeID +=1;
-        if(employeeID < employeeList.length){
-            displayDataModal(employeeList[employeeID][0]);
-            console.log(employeeList[employeeID][0])
-        }else{
-            employeeID = 0;
-        }
-        return employeeID;
+        id++;
+
+        id = id % list.length;  //if there is a remainder of 1 then go back to start
+
+        displayDataModal(id, list[id], list);
     });
 }
-function prevModal(employeeID){
+//check which id is current and which list the filtered list or normal list
+
+function prevModal(id, list){
     $('.prev').click(function(e) {
         e.preventDefault();
-        employeeID -=1;
-        if(employeeID > 0){
-            displayDataModal(employeeList[employeeID][0]);
-            console.log(employeeList[employeeID][0])
-        }else{
-            employeeID = employeeList.length;
+
+        id--;
+        if(id < 0){
+            id = list.length -1;
         }
-        return employeeID;
+        displayDataModal(id, list[id], list);
     });
 }
-
+//get the JSON data and call the getEmployeeData function
 $.getJSON(url, getEmployeeData);
